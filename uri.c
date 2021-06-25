4871b9855e8edd6a7a7d9a5d51245f3eed8f07d1
Hridayshaha <contact.hridayshaha@gmail.com>
2021-06-26 04:51:04 +0600

    Main Branch

--- uri.c
+++ uri.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 int main(){
 
-    double A, B, MEDIA;
-    scanf("%lf %lf", &A, &B);
-
-    MEDIA = ((A*3.5)+(B*7.5))/(3.5+7.5);
-    printf("MEDIA = %0.5lf\n", MEDIA);
+    printf("URI PROBLEM SOLVING\nEvery Problem You Can Find By Branch.");
 
     return 0;
 }
