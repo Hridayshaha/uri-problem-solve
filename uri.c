3c29aced03f1fe4133cd588d93c24542cb5ca3bd
Hridayshaha <contact.hridayshaha@gmail.com>
2021-06-26 20:11:13 +0600

    Uri Problem 1012

--- uri.c
+++ uri.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main(){
 
+    double A, B, C, TRAINGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
     const double PI = 3.14159;
-    double R, VOLUME;
-    scanf("%lf",&R);
-    VOLUME = (4 / 3.0) * PI * (R * R * R);
-    printf("VOLUME = %0.3lf\n",VOLUME);
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    TRAINGULO = ( 1 / 2.0 ) * A * C;
+    CIRCULO = PI * (C * C);
+    TRAPEZIO = ((A + B) / 2.0) * C;
+    QUADRADO = B * B;
+    RETANGULO = A * B;
+
+    printf("TRIANGULO: %0.3lf\nCIRCULO: %0.3lf\nTRAPEZIO: %0.3lf\nQUADRADO: %0.3lf\nRETANGULO: %0.3lf\n", TRAINGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO);
 
     return 0;
 }
