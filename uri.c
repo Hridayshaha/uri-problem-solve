945fe52f297ba390b9066d6bbc7942e7c9370eb8
Hridayshaha <contact.hridayshaha@gmail.com>
2021-06-26 14:39:41 +0600

    Uri Problem 1011

--- uri.c
+++ uri.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 int main(){
 
-    float productPrice1, productPrice2, valueToPay1, valueToPay2, totalValueToPay;
-    int code1, code2, unit1, unit2;
-    scanf("%d %d %f", &code1, &unit1, &productPrice1);
-    scanf("%d %d %f", &code2, &unit2, &productPrice2);
-
-    valueToPay1 = unit1 * productPrice1;
-    valueToPay2 = unit2 * productPrice2;
-
-    totalValueToPay = valueToPay1 + valueToPay2;
-
-    printf("VALOR A PAGAR: R$ %0.2f\n", totalValueToPay);
+    const double PI = 3.14159;
+    double R, VOLUME;
+    scanf("%lf",&R);
+    VOLUME = (4 / 3.0) * PI * (R * R * R);
+    printf("VOLUME = %0.3lf\n",VOLUME);
 
     return 0;
 }
