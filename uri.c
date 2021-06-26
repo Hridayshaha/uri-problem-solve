4c658ebd43efde511c4e105cf490b154e0b5f2ac
Hridayshaha <contact.hridayshaha@gmail.com>
2021-06-26 14:24:16 +0600

    Uri Problem 1010

--- uri.c
+++ uri.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 int main(){
 
-    char name;
-    double fixedSalary, percentOfSale, TOTAL;
-    scanf("%s %lf %lf" ,&name, &fixedSalary, &percentOfSale);
-    TOTAL = fixedSalary + ((percentOfSale * 15) / 100);
-    printf("TOTAL = R$ %0.2lf\n",TOTAL);
+    float productPrice1, productPrice2, valueToPay1, valueToPay2, totalValueToPay;
+    int code1, code2, unit1, unit2;
+    scanf("%d %d %f", &code1, &unit1, &productPrice1);
+    scanf("%d %d %f", &code2, &unit2, &productPrice2);
 
+    valueToPay1 = unit1 * productPrice1;
+    valueToPay2 = unit2 * productPrice2;
+
+    totalValueToPay = valueToPay1 + valueToPay2;
+
+    printf("VALOR A PAGAR: R$ %0.2f\n", totalValueToPay);
 
     return 0;
 }
